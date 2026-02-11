7541297e41cc3d508e941bf40ac52f7d7d300c61
Ansh-mick27 <anshuloza@acropolis.in>
2026-02-11 15:59:02 +0530

    Increase spacing between Legacy Allies scrolling rows

--- src/components/LegacyAllies.tsx
+++ src/components/LegacyAllies.tsx
@@ -35,7 +35,7 @@ export default function LegacyAllies() {
                 </motion.div>
 
                 {/* Scrolling Rows */}
-                <div className="space-y-4">
+                <div className="space-y-12">
                     {/* Row 1 - Scroll Left */}
                     <div
                         className="relative overflow-hidden"
