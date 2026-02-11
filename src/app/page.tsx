8d26b7fc9a52607da5c235b88736d49a6d499d5a
Ansh-mick27 <anshuloza@acropolis.in>
2026-02-11 16:22:24 +0530

    Add spiral staircase repeating background on right side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,12 @@ import PoweredBySection from "@/components/PoweredBySection";
 import LegacyAllies from "@/components/LegacyAllies";
 import OfficialPartners from "@/components/OfficialPartners";
 import EnhancedFooter from "@/components/EnhancedFooter";
+import SpiralBackground from "@/components/SpiralBackground";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-bg-primary text-white relative">
+      <SpiralBackground />
       <CyberpunkNav />
       <CyberpunkHero />
       <EventsGrid />
---
+++ src/components/SpiralBackground.tsx
@@ -0,0 +1,14 @@
+export default function SpiralBackground() {
+    return (
+        <div
+            className="fixed top-0 right-0 bottom-0 w-[25vw] -z-10 pointer-events-none"
+            style={{
+                backgroundImage: 'url(/images/spiral-stairs.png)',
+                backgroundRepeat: 'repeat-y',
+                backgroundPosition: 'right center',
+                backgroundSize: 'contain',
+                opacity: 0.15
+            }}
+        />
+    );
+}
