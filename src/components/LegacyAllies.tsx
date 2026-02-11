fce3edf5a803b2a89241b4fffe7cd472a97bc2be
Ansh-mick27 <anshuloza@acropolis.in>
2026-02-11 16:08:58 +0530

    Dramatically increase row spacing to 10rem for maximum separation

--- src/components/LegacyAllies.tsx
+++ src/components/LegacyAllies.tsx
@@ -35,7 +35,7 @@ export default function LegacyAllies() {
                 </motion.div>
 
                 {/* Scrolling Rows */}
-                <div className="space-y-24">
+                <div className="space-y-40">
                     {/* Row 1 - Scroll Left */}
                     <div
                         className="relative overflow-hidden"
