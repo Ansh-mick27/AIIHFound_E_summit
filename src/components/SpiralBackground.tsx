9c284d25fcfb162138793225cbdc0847f07d8534
Ansh-mick27 <anshuloza@acropolis.in>
2026-02-11 16:30:51 +0530

    Add vertical scrolling animation to spiral background

--- src/components/SpiralBackground.tsx
+++ src/components/SpiralBackground.tsx
@@ -1,14 +1,27 @@
 export default function SpiralBackground() {
     return (
-        <div
-            className="fixed top-0 right-0 bottom-0 w-[25vw] -z-10 pointer-events-none"
-            style={{
-                backgroundImage: 'url(/images/spiral-stairs.png)',
-                backgroundRepeat: 'repeat-y',
-                backgroundPosition: 'right center',
-                backgroundSize: 'contain',
-                opacity: 0.5
-            }}
-        />
+        <>
+            <style jsx>{`
+        @keyframes spiralScroll {
+          0% {
+            background-position: right 0%;
+          }
+          100% {
+            background-position: right 100%;
+          }
+        }
+      `}</style>
+            <div
+                className="fixed top-0 right-0 bottom-0 w-[25vw] -z-10 pointer-events-none"
+                style={{
+                    backgroundImage: 'url(/images/spiral-stairs.png)',
+                    backgroundRepeat: 'repeat-y',
+                    backgroundPosition: 'right center',
+                    backgroundSize: 'contain',
+                    opacity: 0.5,
+                    animation: 'spiralScroll 20s linear infinite'
+                }}
+            />
+        </>
     );
 }
