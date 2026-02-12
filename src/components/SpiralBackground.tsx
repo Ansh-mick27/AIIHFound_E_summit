9c2a23dca8dc21d6bbe72a75ede3a2965355a720
Ansh-mick27 <anshuloza@acropolis.in>
2026-02-12 15:03:05 +0530

    Increase all spiral background opacity to 100% and update ACRO-LOGO

--- src/components/SpiralBackground.tsx
+++ src/components/SpiralBackground.tsx
@@ -1,7 +1,7 @@
 export default function SpiralBackground() {
-    return (
-        <>
-            <style jsx>{`
+  return (
+    <>
+      <style jsx>{`
         @keyframes spiralScroll {
           0% {
             background-position: right 0%;
@@ -11,17 +11,17 @@ export default function SpiralBackground() {
           }
         }
       `}</style>
-            <div
-                className="fixed top-0 right-0 bottom-0 w-[25vw] -z-10 pointer-events-none"
-                style={{
-                    backgroundImage: 'url(/images/spiral-stairs.png)',
-                    backgroundRepeat: 'repeat-y',
-                    backgroundPosition: 'right center',
-                    backgroundSize: 'contain',
-                    opacity: 0.5,
-                    animation: 'spiralScroll 20s linear infinite'
-                }}
-            />
-        </>
-    );
+      <div
+        className="fixed top-0 right-0 bottom-0 w-[25vw] -z-10 pointer-events-none"
+        style={{
+          backgroundImage: 'url(/images/spiral-stairs.png)',
+          backgroundRepeat: 'repeat-y',
+          backgroundPosition: 'right center',
+          backgroundSize: 'contain',
+          opacity: 1.0,
+          animation: 'spiralScroll 20s linear infinite'
+        }}
+      />
+    </>
+  );
 }
